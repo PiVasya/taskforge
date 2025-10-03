8821e2a2b5ed82b14b028b87460585de0ac11c17
Матвей Васильев <vasyateranit@gmail.com>
2025-10-03 22:41:29 +0300

    ...

--- csharp-runner/Program.cs
+++ csharp-runner/Program.cs
@@ -1,7 +1,6 @@
 using Runner.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
