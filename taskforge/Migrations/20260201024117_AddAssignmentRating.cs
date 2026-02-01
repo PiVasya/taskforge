da79393dee3d0d96b37393346831431c35bd55a1
PiVasya <vasyateranit@gmail.com>
2026-02-01 05:41:33 +0300

    ...

---
+++ taskforge/Migrations/20260201024117_AddAssignmentRating.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taskforge.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAssignmentRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "TaskAssignments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "TaskAssignments");
+        }
+    }
+}
