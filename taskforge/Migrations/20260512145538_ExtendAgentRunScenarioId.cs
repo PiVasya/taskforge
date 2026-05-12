ffbc645b57f3e722c3c03d4cc6f79202b4c58644
Матвей Васильев Ноутбук Linux <vasyateranit@gmail.com>
2026-05-12 17:55:49 +0300

    ...

---
+++ taskforge/Migrations/20260512145538_ExtendAgentRunScenarioId.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taskforge.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendAgentRunScenarioId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ScenarioId",
+                table: "AgentRuns",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldMaxLength: 80,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ScenarioId",
+                table: "AgentRuns",
+                type: "character varying(80)",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
